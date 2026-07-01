0b75284be3bae90984a205e06956451c366a12a3
gundars1 <64661410+gundars1@users.noreply.github.com>
2026-07-01 10:00:46 +0300

    Update __init__.py

--- components/mppt_reader/__init__.py
+++ components/mppt_reader/__init__.py
@@ -1,13 +1,9 @@
-async def to_code(config):
-    # 1. Vispirms paņemam mainīgos no konfigurācijas
-    uart_component = await cg.get_variable(config[CONF_UART_ID])
-    de_pin = await cg.gpio_pin_expression(config["de_pin"])
+import esphome.codegen as cg
+import esphome.config_validation as cv
 
-    # 2. Nododam tos tieši C++ konstruktoram, kad veidojam jauno objektu (MpptReader(uart, de_pin))
-    var = cg.new_Pvariable(config[CONF_ID], uart_component, de_pin)
-    await cg.register_component(var, config)
+# Definējam C++ namespace un komponentes identifikāciju
+mppt_reader_ns = cg.esphome_ns.namespace("mppt_reader")
+MpptReaderComponent = mppt_reader_ns.class_("MpptReader", cg.Component)
 
-    # 3. Piereģistrējam sensorus (tas paliek nemainīgs)
-    for key in SENSORS:
-        sens = await sensor.new_sensor(config[key])
-        cg.add(getattr(var, f"set_{key}_sensor")(sens))
+# Tukša shēma, jo reālo YAML konfigurāciju apstrādās mppt_reader.py failā
+CONFIG_SCHEMA = cv.Schema({})
